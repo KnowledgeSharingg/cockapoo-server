e3fadbe150d15e74e27575fe77698684c0541476
limseongkyu <96x60812@gmail.com>
2023-04-29 17:08:10 +0900

    Setting: env

---
+++ src/test/java/com/alan/cockapooserver/CockapooServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.cockapooserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CockapooServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
