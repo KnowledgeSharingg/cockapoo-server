912760d8707ec5dfa1ddfeaa6a2789b5029de45d
seongkyulim <96x60812@gmail.com>
2024-12-09 17:38:20 +0900

    feat: securityFilterChain

---
+++ src/main/kotlin/com/example/cockapoo/cockapoo_server/filter/SecurityFilterChain.kt
@@ -0,0 +1,15 @@
+
+
+// import org.springframework.context.annotation.Bean
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity
+// import org.springframework.security.web.SecurityFilterChain
+
+
+// @Bean
+// fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+//     http.csrf().disable()
+//         .authorizeHttpRequests {
+//             it.anyRequest().permitAll()
+//         }
+//     return http.build()
+// }
