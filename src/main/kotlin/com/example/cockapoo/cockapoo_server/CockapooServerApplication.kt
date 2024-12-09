931aa650da4d120e1aa8eb8c16860436b4259a8f
seongkyulim <96x60812@gmail.com>
2024-12-09 17:44:50 +0900

    feat: sentry setting

--- src/main/kotlin/com/example/cockapoo/cockapoo_server/CockapooServerApplication.kt
+++ src/main/kotlin/com/example/cockapoo/cockapoo_server/CockapooServerApplication.kt
@@ -2,7 +2,13 @@ package com.example.cockapoo.cockapoo_server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import io.sentry.spring.jakarta.EnableSentry
+import org.springframework.core.Ordered
 
+@EnableSentry(
+  dsn = "https://dd648c43ed4002ece0303dc51fbf07b8@o4508437281832960.ingest.us.sentry.io/4508437284061184",
+  exceptionResolverOrder = Ordered.LOWEST_PRECEDENCE
+)
 @SpringBootApplication
 class CockapooServerApplication
 
